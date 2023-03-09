fbe5a7627b629a9400c2d7e7099c1934b592e355
lixiaoyong <lixiaoyong19@huawei.com>
2023-03-09 11:31:31 +0800

    yocto: delete unnecessary files and directory
    * delete unnecessary files and directory, which have been
      taransferred to yocto-meta-openeuler
    
    Signed-off-by: lixiaoyong <lixiaoyong19@huawei.com>

--- build_tools/yocto-kernel-tools/tools/scc-cmds/define.cmd
+++
@@ -1,9 +0,0 @@
-define() {
-    local var=$1
-    local value="$2"
-    local text="# _define $var '$value'"
-
-    # make the variable real
-    eval $var="\"$value\""
-    eval echo "\$text" $outfile_append
-}
--- build_tools/yocto-kernel-tools/tools/scc-cmds/mark.cmd
+++
@@ -1,4 +0,0 @@
-mark() {
-       local text="# _mark $@"
-       eval echo \$text  $outfile_append
-}
